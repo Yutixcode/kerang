df68c3a03d5d4f13a4701d081836018852aca8a8
Yutixcode <yutixcode@gmail.com>
2023-08-04 17:10:18 +0700

    update

--- list.php
+++ list.php
@@ -1,5 +1,9 @@
 <?php
-$list = scandir(".");
-$link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-echo $link;
+echo '<html><meta name="viewport" content="width=device-width, initial-scale=1.0"><table>';
+$files = scandir('.');
+foreach($files as $file) {
+	$uri = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	$url = str_replace("list.php",$file,$uri);
+	echo "<tr><td>" . filesize($file) . "</td><td>" . '<a href="' . $url . '">' . $file . "</a>" . "</td></tr>";
+}
 ?>
